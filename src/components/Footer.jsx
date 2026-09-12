22cecd5c672d85171627336adefb164850967257
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-12 08:21:23 +0330

    Structure component tree

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>&copy; 2077. Copyright by NightCity.</small>
+            <p>
+                Version <b>7.7</b>
+            </p>
+        </footer>
+    );
+}
