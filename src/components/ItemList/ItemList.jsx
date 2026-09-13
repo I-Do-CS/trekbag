27998ce9eba2ca945e8e7b08165004a10b584bee
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-13 04:16:02 +0330

    Add an empty view to the ItemList

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,12 +1,15 @@
+import EmptyView from "./EmptyView";
 import Item from "./Item";
 
 function ItemList({ items, operations }) {
     return (
-        <ul>
-            {items.map((item) => (
-                <Item key={item.id} item={item} operations={operations} />
-            ))}
-        </ul>
+        <>
+            <ul className="item-list">
+                {items.length === 0 && <EmptyView />}
+                {items.length > 0 &&
+                    items.map((item) => <Item key={item.id} item={item} operations={operations} />)}
+            </ul>
+        </>
     );
 }
 
