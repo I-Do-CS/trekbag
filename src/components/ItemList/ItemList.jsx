3886c121224d2241b21fd707f3535320a1c80463
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-13 06:17:49 +0330

    Add sorting functionality

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,13 +1,42 @@
+import Select from "react-select";
 import EmptyView from "./EmptyView";
 import Item from "./Item";
+import { SORT_OPTIONS } from "../../constants";
+import { useMemo, useState } from "react";
 
 function ItemList({ items, operations }) {
+    const itemsIsEmpty = items.length === 0;
+    const sortOptions = [...SORT_OPTIONS];
+    const [sortBy, setSortBy] = useState(sortOptions[0].value);
+    const sortedItems = useMemo(() => {
+        return [...items].sort((a, b) => {
+            if (sortBy === "default") return 0;
+            if (sortBy === "packed") return b.packed - a.packed;
+            if (sortBy === "unpacked") return a.packed - b.packed;
+        });
+    }, [items, sortBy]);
+
+    const handleSortChange = (option) => {
+        setSortBy(option.value);
+    };
+
     return (
         <>
             <ul className="item-list">
-                {items.length === 0 && <EmptyView />}
-                {items.length > 0 &&
-                    items.map((item) => <Item key={item.id} item={item} operations={operations} />)}
+                {itemsIsEmpty && <EmptyView />}
+                {!itemsIsEmpty && (
+                    <section className="sorting">
+                        <Select
+                            onChange={handleSortChange}
+                            defaultValue={sortOptions[0]}
+                            options={sortOptions}
+                        />
+                    </section>
+                )}
+                {!itemsIsEmpty &&
+                    sortedItems.map((item) => (
+                        <Item key={item.id} item={item} operations={operations} />
+                    ))}
             </ul>
         </>
     );
--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,9 @@ export const SEED_ITEMS = [
     { id: "seed-item-2", text: "passport", packed: false },
     { id: "seed-item-3", text: "phone charger", packed: false },
 ];
+
+export const SORT_OPTIONS = [
+    { label: "Sort by Default", value: "default" },
+    { label: "Sort by Packed", value: "packed" },
+    { label: "Sort by Unpacked", value: "unpacked" },
+];
